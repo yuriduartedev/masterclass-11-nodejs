bfa4fc23bd82fa53e5bccb6d9ad8f1af35306ea6
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-12 16:39:19 -0300

    Create logger

--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const os = require('os');
+const log = require('./logger');
 
 setInterval(() => {
   const { freemem, totalmem } = os
@@ -16,4 +17,5 @@ setInterval(() => {
   console.clear();
   console.log('====== PC STATS ======');  
   console.table(stats);
+  log(`${JSON.stringify(stats)}\n`);
 }, 1000)
